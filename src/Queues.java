b7b7273fd7e96cbb82222e39578e70ee3a65c56d
rahilvora <rahil.vora740@gmail.com>
2015-02-04 02:06:46 +0530

    pop and push function structure added

--- src/Queues.java
+++ src/Queues.java
@@ -3,6 +3,14 @@ public class Queues {
 
 	public static void main(String[] args)
 	{
+		System.out.println("Today we will implement a new data structure Queues");
+	}
+	public void pop()
+	{
+		
+	}
+	public void push()
+	{
 		
 	}
 }
