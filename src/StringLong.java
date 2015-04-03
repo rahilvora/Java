633e4026dec53cfc33d03b17bee6a6d48ca4cbb9
Rahil Vora <rahil.vora740@gmail.com>
2015-04-03 11:36:40 +0530

    Uodated StringLong file

--- src/StringLong.java
+++ src/StringLong.java
@@ -2,17 +2,17 @@ public class StringLong {
 	// 9223372036854775807 -9223372036854775808
 	public static void main(String args[]) {
 		long result;
-		String sample = "-92";
+		String sample = "-92233720368547758088";
 		int a = sample.charAt(sample.length() - 1);
 		if (sample.substring(0, 1).matches("-") && sample.length() < 21) {
 			if (sample.length() == 20 && a <= 56) {
-				stringToLong(sample,-1);
+				stringToLong(sample.substring(1),-1);
 			}
 			 else if (sample.length() == 20 && a > 56) {
 					System.out.println("Invalid String");
 					
-				} else if (sample.length() < 120)
-					stringToLong(sample,-1);
+				} else if (sample.length() < 20)
+					stringToLong(sample.substring(1),-1);
 				else
 					System.out.println("Invalid String");
 
@@ -41,14 +41,20 @@ public class StringLong {
 				result = result * 10 + (sample.charAt(i) - 48);
 				count++;
 			}
+			else{
+				System.out.println("Invalid input");
+				//break;
+				return;
+			}
 
 		}
-		if (Long.MIN_VALUE <= result && result <= Long.MAX_VALUE) {
+		System.out.print(a*result + " String -> Long");
+		/*if (Long.MIN_VALUE <= result && result <= Long.MAX_VALUE) {
 			System.out.print(a*result+"  String->Long");
 		}
 		else {
 			System.out.println("Invalid input");
-		}
+		}*/
 
 	}
 }
